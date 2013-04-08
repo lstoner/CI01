9131acac55e55624e8826fcaca3dc37c68026c80
lstoner <lstoner@sustainablesv.org>
2013-04-07 20:41:08 -0700

    linked blog module to post

--- application/models/blog_model.php
+++ application/models/blog_model.php
@@ -1,28 +1,43 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Blog_model extends CI_Model
-{
-    function __construct()
-    {
-        parent::__construct();
-        $this->load->database();
-    }
- 
-    function get_all_posts()
-    {
-        //get all entry
-        $query = $this->db->get('entry');
-        return $query->result();
-    }
- 
-    function add_new_entry($name,$body)
-    {
-        $data = array(
-            'entry_name' => $name,
-            'entry_body' => $body
-        );
-        $this->db->insert('entry',$data);
-    }
+class Blog_model extends CI_Model {
+
+	function get($id) {
+		$this->db->where('id', $id);
+		$query = $this->db->get('comments');
+		return $query->row_array();
+	}
+	
+	function get_all() {
+		$this->db->order_by('date_created','desc');
+		$query = $this->db->get('comments');
+		return $query->result_array();
+	}
+	
+	function insert($post_id, $title, $email, $body) {
+		$date = date('Y-m-d H:i:s');
+		$data = array(
+				'post_id' => $post_id,
+				'title' => $title,
+				'email' => $email,
+				'body' => $body,
+				'date_created' => $date,
+				'last_updated' => $date
+		);
+		$this->db->insert('comments', $data);
+	}
+	
+	public function update() {
+		$this->title = $_POST['title'];
+		$this->body = $_POST['body'];
+		$this->last_updated = date('Y-m-d H:i:s');
+			
+		$this->db->update('comments', $this, array('id' => $_POST['id']));
+	}
+	
+	public function delete() {
+		$this->db->delete('comments', array('id' => $_POST['id']));
+	}
+	
 }
- 
-/* End of file blog_model.php */
+	/* End of file blog_model.php */
 /* Location: ./application/models/blog_model.php */
--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
-drop table if exists comments;
+drop table if exists blogs;
 drop table if exists posts;
+drop table if exists comments;
+
+create table blogs (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  date_created datetime NOT NULL,
+  last_updated datetime NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 create table posts (
   id int(11) NOT NULL AUTO_INCREMENT,
+--  blog_id int(11) NOT NULL,
   title varchar(255) NOT NULL,
   body text NOT NULL,
   image varchar(255) NOT NULL,
